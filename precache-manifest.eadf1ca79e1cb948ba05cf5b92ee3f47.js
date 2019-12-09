da0c6376c0c421081e5ede70fa3c4a3ef1fc6c62
natya2203 <nastya_2203@ukr.net>
2019-12-10 00:37:17 +0200

    Updates

---
+++ precache-manifest.eadf1ca79e1cb948ba05cf5b92ee3f47.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c897b5dd517430267744acced2b26e7d",
+    "url": "/react_list-of-goods/index.html"
+  },
+  {
+    "revision": "5a24a4d87377b5283f3c",
+    "url": "/react_list-of-goods/static/js/2.4a80f5eb.chunk.js"
+  },
+  {
+    "revision": "1c4443c8f0022d81de8c",
+    "url": "/react_list-of-goods/static/js/main.e7520d91.chunk.js"
+  },
+  {
+    "revision": "e82fdff4b55e53116e45",
+    "url": "/react_list-of-goods/static/js/runtime~main.14fd7ef8.js"
+  }
+]);
